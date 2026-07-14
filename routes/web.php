ab94193aeb64050621d2680fe0c4aade0ec1d4b6
hekalrapli <haikalrfli11@gmail.com>
2026-07-14 15:20:18 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DistribusiController;
+use App\Http\Controllers\PenimbanganController;
+use App\Http\Controllers\PilahSampahController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::resource('penimbangan', PenimbanganController::class);
+Route::resource('pilah-sampah', PilahSampahController::class);
+Route::resource('distribusi', DistribusiController::class);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('dashboard');
